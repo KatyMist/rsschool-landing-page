e4089e67c3fe0b66681c2d5545666b2424c3bb5d
Ekaterina Tumanova <tumanova.ekaterina73@gmail.com>
2026-09-22 15:22:40 +0400

    fix(catalog): import product images as ES modules for production build

--- src/data/products.js
+++ src/data/products.js
@@ -1,3 +1,24 @@
+import coffee10Img from '../assets/image/coffee-10.png';
+import coffee11Img from '../assets/image/coffee-11.png';
+import coffee4Img from '../assets/image/coffee-4.png';
+import coffee5Img from '../assets/image/coffee-5.png';
+import coffee6Img from '../assets/image/coffee-6.png';
+import coffee7Img from '../assets/image/coffee-7.png';
+import coffee8Img from '../assets/image/coffee-8.png';
+import coffee9Img from '../assets/image/coffee-9.png';
+import dessert1Img from '../assets/image/dessert-1.png';
+import dessert2Img from '../assets/image/dessert-2.png';
+import dessert3Img from '../assets/image/dessert-3.png';
+import dessert4Img from '../assets/image/dessert-4.png';
+import dessert5Img from '../assets/image/dessert-5.png';
+import dessert6Img from '../assets/image/dessert-6.png';
+import dessert7Img from '../assets/image/dessert-7.png';
+import dessert8Img from '../assets/image/dessert-8.png';
+import tea1Img from '../assets/image/tea-1.png';
+import tea2Img from '../assets/image/tea-2.png';
+import tea3Img from '../assets/image/tea-3.png';
+import tea4Img from '../assets/image/tea-4.png';
+
 // Схема повторяет официальный products.json задания (RS School, Coffee House):
 // у каждого размера и у каждой добавки своя доплата (add-price), из которых
 // в модалке складывается итоговая цена. Базовая цена (price) соответствует
@@ -39,7 +60,7 @@ export const products = [
     name: 'Irish coffee',
     desc: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
     price: 7.0,
-    image: '/src/assets/image/coffee-4.png',
+    image: coffee4Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -49,7 +70,7 @@ export const products = [
     name: 'Kahlua coffee',
     desc: 'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
     price: 7.0,
-    image: '/src/assets/image/coffee-5.png',
+    image: coffee5Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -59,7 +80,7 @@ export const products = [
     name: 'Honey raf',
     desc: 'Espresso with frothed milk, cream and aromatic honey',
     price: 5.5,
-    image: '/src/assets/image/coffee-6.png',
+    image: coffee6Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -69,7 +90,7 @@ export const products = [
     name: 'Ice cappuccino',
     desc: 'Cappuccino with soft thick foam in summer version with ice',
     price: 5.0,
-    image: '/src/assets/image/coffee-7.png',
+    image: coffee7Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -79,7 +100,7 @@ export const products = [
     name: 'Espresso',
     desc: 'Classic black coffee',
     price: 4.5,
-    image: '/src/assets/image/coffee-8.png',
+    image: coffee8Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -89,7 +110,7 @@ export const products = [
     name: 'Latte',
     desc: 'Espresso coffee with the addition of steamed milk and dense milk foam',
     price: 5.5,
-    image: '/src/assets/image/coffee-9.png',
+    image: coffee9Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -99,7 +120,7 @@ export const products = [
     name: 'Latte macchiato',
     desc: 'Espresso with frothed milk and chocolate',
     price: 5.5,
-    image: '/src/assets/image/coffee-10.png',
+    image: coffee10Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -109,7 +130,7 @@ export const products = [
     name: 'Coffee with cognac',
     desc: 'Fragrant black coffee with cognac and whipped cream',
     price: 6.5,
-    image: '/src/assets/image/coffee-11.png',
+    image: coffee11Img,
     sizes: COFFEE_SIZES,
     additives: COFFEE_ADDITIVES,
   },
@@ -119,7 +140,7 @@ export const products = [
     name: 'Moroccan',
     desc: 'Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint',
     price: 4.5,
-    image: '/src/assets/image/tea-1.png',
+    image: tea1Img,
     sizes: COFFEE_SIZES,
     additives: TEA_ADDITIVES,
   },
@@ -129,7 +150,7 @@ export const products = [
     name: 'Ginger',
     desc: 'Original black tea with fresh ginger, lemon and honey',
     price: 5.0,
-    image: '/src/assets/image/tea-2.png',
+    image: tea2Img,
     sizes: COFFEE_SIZES,
     additives: TEA_ADDITIVES,
   },
@@ -139,7 +160,7 @@ export const products = [
     name: 'Cranberry',
     desc: 'Invigorating black tea with cranberry and honey',
     price: 5.0,
-    image: '/src/assets/image/tea-3.png',
+    image: tea3Img,
     sizes: COFFEE_SIZES,
     additives: TEA_ADDITIVES,
   },
@@ -149,7 +170,7 @@ export const products = [
     name: 'Sea buckthorn',
     desc: 'Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon',
     price: 5.5,
-    image: '/src/assets/image/tea-4.png',
+    image: tea4Img,
     sizes: COFFEE_SIZES,
     additives: TEA_ADDITIVES,
   },
@@ -159,7 +180,7 @@ export const products = [
     name: 'Marble cheesecake',
     desc: 'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
     price: 3.5,
-    image: '/src/assets/image/dessert-1.png',
+    image: dessert1Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -169,7 +190,7 @@ export const products = [
     name: 'Red velvet',
     desc: 'Layer cake with cream cheese frosting',
     price: 4.0,
-    image: '/src/assets/image/dessert-2.png',
+    image: dessert2Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -179,7 +200,7 @@ export const products = [
     name: 'Cheesecakes',
     desc: 'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
     price: 4.5,
-    image: '/src/assets/image/dessert-3.png',
+    image: dessert3Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -189,7 +210,7 @@ export const products = [
     name: 'Creme brulee',
     desc: 'Delicate creamy dessert in a caramel basket with wild berries',
     price: 4.0,
-    image: '/src/assets/image/dessert-4.png',
+    image: dessert4Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -199,7 +220,7 @@ export const products = [
     name: 'Pancakes',
     desc: 'Tender pancakes with strawberry jam and fresh strawberries',
     price: 4.5,
-    image: '/src/assets/image/dessert-5.png',
+    image: dessert5Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -209,7 +230,7 @@ export const products = [
     name: 'Honey cake',
     desc: 'Classic honey cake with delicate custard',
     price: 4.5,
-    image: '/src/assets/image/dessert-6.png',
+    image: dessert6Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -219,7 +240,7 @@ export const products = [
     name: 'Chocolate cake',
     desc: 'Cake with hot chocolate filling and nuts with dried apricots',
     price: 5.5,
-    image: '/src/assets/image/dessert-7.png',
+    image: dessert7Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
@@ -229,7 +250,7 @@ export const products = [
     name: 'Black forest',
     desc: 'A combination of thin sponge cake with cherry jam and light chocolate mousse',
     price: 6.5,
-    image: '/src/assets/image/dessert-8.png',
+    image: dessert8Img,
     sizes: DESSERT_SIZES,
     additives: DESSERT_ADDITIVES,
   },
